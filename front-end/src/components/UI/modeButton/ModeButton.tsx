53c4e391fefc9223e68383f5ac7cc61810aad533
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-09-30 15:49:07 +0530

    added mode toggle logic

--- front-end/src/components/UI/modeButton/ModeButton.tsx
+++ front-end/src/components/UI/modeButton/ModeButton.tsx
@@ -1,18 +1,52 @@
+"use client";
+
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import { useEffect, useState } from "react";
 import { DarkIcon, LightIcon } from "@/components/Icons";
 import "./mode-button.css";
 
 function ModeButton() {
+  const [mode, setMode] = useState(localStorage.getItem("mode"));
+
+  const changeMode = () => {
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
+  };
+
+  useEffect(() => {
+    if (!mode) {
+      const m = window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
+      setMode(m);
+    } else {
+      localStorage.setItem("mode", mode);
+      document.documentElement.setAttribute("data-theme", mode);
+      document.body.classList.add(mode === "dark" ? "dark-mode" : "light-mode");
+      document.body.classList.remove(
+        mode !== "dark" ? "dark-mode" : "light-mode",
+      );
+    }
+  }, [mode]);
+
   return (
-    <div className="mode-button">
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+    <div
+      onClick={() => changeMode()}
+      className="mode-button"
+      title={`Turn On ${mode === "dark" ? "Light" : "Dark"} Mode`}
+      aria-label="toggle dark mode"
+    >
       <label htmlFor="mode">
-        <input type="checkbox" name="mode" id="mode" />
         <div className="icons">
-          <span id="dark-icon">
-            <DarkIcon />
-          </span>
-          <span id="light-icon">
-            <LightIcon />
-          </span>
+          {mode === "dark" ? (
+            <span id="light-icon">
+              <LightIcon />
+            </span>
+          ) : (
+            <span id="dark-icon">
+              <DarkIcon />
+            </span>
+          )}
         </div>
       </label>
     </div>
