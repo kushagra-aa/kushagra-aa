d1344b984be37b2d39a7aee965b3e569cd2637ec
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-10-01 00:22:13 +0530

    added button component

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import Button from "@/components/UI/buttons/Buttons";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -5,6 +8,28 @@ export default function Home() {
     <div className={styles.main}>
       <h1>hello</h1>
       <p>hello sub</p>
+      <Button onClick={() => {}} size="small" theme="theme-1">
+        Hello
+      </Button>
+      <Button onClick={() => {}} size="medium" theme="theme-2">
+        Hello
+      </Button>
+      <Button
+        onClick={() => {}}
+        size="large"
+        backgroundColor="accent-2"
+        foregroundColor="dark-2"
+      >
+        Hello
+      </Button>
+      <Button
+        onClick={() => {}}
+        size="large"
+        backgroundColor="accent-1"
+        foregroundColor="light-1"
+      >
+        Hello
+      </Button>
     </div>
   );
 }
--- front-end/src/components/UI/buttons/Buttons.tsx
+++ front-end/src/components/UI/buttons/Buttons.tsx
@@ -1,7 +1,52 @@
+/* eslint-disable react/require-default-props */
 import "./buttons.css";
 
-function Button() {
-  return <button type="button">Buttons</button>;
+type Colors =
+  | "dark-1"
+  | "dark-2"
+  | "light-1"
+  | "light-2"
+  | "accent-1"
+  | "accent-2";
+
+type ButtonPropType = {
+  children: React.ReactNode;
+  onClick: () => void;
+  backgroundColor?: Colors;
+  foregroundColor?: Colors;
+  strokeColor?: Colors;
+  size: "small" | "medium" | "large";
+  stoke?: "stroke-1" | "stroke-2" | "stroke-3" | "stroke-4";
+  theme?: "theme-1" | "theme-2";
+  title?: string;
+  className?: string;
+  style?: {};
+};
+
+function Button({
+  children,
+  title,
+  onClick,
+  style,
+  className,
+  backgroundColor,
+  foregroundColor,
+  strokeColor,
+  size,
+  theme,
+  stoke,
+}: ButtonPropType) {
+  return (
+    <button
+      title={title}
+      onClick={() => onClick()}
+      style={style}
+      className={`${className} ${theme}-button background-${backgroundColor} foreground-${foregroundColor} stroke-${strokeColor} ${stoke} button-${size} button`}
+      type="button"
+    >
+      {children}
+    </button>
+  );
 }
 
 export default Button;
