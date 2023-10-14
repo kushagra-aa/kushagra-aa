6f7919815c463db59bc1900cff98efa1f58e89e5
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-10-14 12:13:30 +0530

    added search component and fixed clear button styles

---
+++ front-end/src/components/UI/search/Search.tsx
@@ -0,0 +1,51 @@
+import Input from "@/components/UI/input/Input";
+import InputGroup from "@/components/UI/input/InputGroup";
+import { CloseIcon, SearchIcon } from "@/components/Icons";
+import Button from "@/components/UI/button/Button";
+import "./search.css";
+
+export default function Search({
+  onSearch,
+  value,
+}: {
+  value?: string;
+  onSearch: (val?: string) => void;
+}) {
+  const handleSearch = () => {
+    onSearch(value);
+  };
+  const handleSearchClear = () => {
+    onSearch(undefined);
+  };
+
+  return (
+    <InputGroup className="search" title="" inputName="search">
+      <Button
+        onClick={handleSearch}
+        className="search_button"
+        size="small"
+        type="button"
+      >
+        <SearchIcon />
+      </Button>
+      <Input
+        placeholder="Search"
+        inputName="search"
+        id="search"
+        type="text"
+        value={value || ""}
+        onChange={(e) => {
+          onSearch(e.target.value);
+        }}
+      />
+      <Button
+        onClick={handleSearchClear}
+        className="search_clear_button"
+        size="small"
+        type="button"
+      >
+        <CloseIcon />
+      </Button>
+    </InputGroup>
+  );
+}
