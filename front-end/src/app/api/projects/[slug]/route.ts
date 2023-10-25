91f41b90527d5b4df589d649280ce059d79c8204
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-10-25 13:13:16 +0530

    added basic get function to api routes

---
+++ front-end/src/app/api/projects/[slug]/route.ts
@@ -0,0 +1,17 @@
+/* eslint-disable import/prefer-default-export */
+import { NextRequest, NextResponse } from "next/server";
+import PROJECTS from "@/dummyData/projects.json";
+import ProjectType from "@/models/Project";
+
+// eslint-disable-next-line unused-imports/no-unused-vars, @typescript-eslint/no-unused-vars
+export async function GET(request: NextRequest) {
+  try {
+    const data: ProjectType[] = PROJECTS;
+    return NextResponse.json({
+      message: "Posts found",
+      data,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
